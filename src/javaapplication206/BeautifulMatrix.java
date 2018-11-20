b82b5942a58dff7c3bdcaa4f3519c7f548337cbc
Darag1996 <moha.darag@yahoo.com>
2018-11-20 23:06:16 +0200

    toString override

--- src/javaapplication206/BeautifulMatrix.java
+++ src/javaapplication206/BeautifulMatrix.java
@@ -17,7 +17,7 @@ public class BeautifulMatrix {
     private final int beautifulMatrixColumn = 3;
     private int oneCurrentRow = 0;
     private int oneCurrentColumn = 0;
-    private int [][] matrix = new int [5][5];
+    private int[][] matrix = new int[5][5];
 
     public BeautifulMatrix() {
         getMatrix();
@@ -28,44 +28,44 @@ public class BeautifulMatrix {
         int oneCounter = 0;
         for (int rows = 0; rows < 5; rows++) {
             for (int columns = 0; columns < 5; columns++) {
-                    matrix[rows][columns] = input.nextInt();
-                    System.out.print("\t");
-              if (matrix[rows][columns] == 1 && oneCounter < 1) {
+                matrix[rows][columns] = input.nextInt();
+                if (matrix[rows][columns] == 1 && oneCounter < 1) {
                     oneCounter++;
-                    oneCurrentRow = rows+1;
-                    oneCurrentColumn = columns+1;
-                //  System.out.println("Row" + oneCurrentRow + "Column" + oneCurrentColumn );
-              } else {
-                  matrix[rows][columns] = 0;
-              }
+                    oneCurrentRow = rows + 1;
+                    oneCurrentColumn = columns + 1;
+                    //  System.out.println("Row" + oneCurrentRow + "Column" + oneCurrentColumn );
+                } else {
+                    matrix[rows][columns] = 0;
+                }
             }
-            System.out.println();
         }
     }
-    
+
     public void printMatrix() {
-         System.out.println("Your Matrix is : ");
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                System.out.print(matrix[i][j]+"\t");
+        System.out.println("Your Matrix is : ");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matrix[i][j] + "\t");
             }
-             
+
             System.out.println();
         }
     }
-    
 
     public int minimumMovesToBeautifulMatrix() {
-      return Math.abs(oneCurrentRow - beautifulMatrixRow) +  Math.abs(oneCurrentColumn - beautifulMatrixColumn);
+        return Math.abs(oneCurrentRow - beautifulMatrixRow) + Math.abs(oneCurrentColumn - beautifulMatrixColumn);
+    }
+
+    @Override
+    public String toString() {
+        return "Minimum moves needed to become a beautiful matrix is " + minimumMovesToBeautifulMatrix();
     }
     
     
 
     public static void main(String[] args) {
         BeautifulMatrix obj = new BeautifulMatrix();
-        System.out.println("MiniMum Moves is " + obj.minimumMovesToBeautifulMatrix());
+        System.out.println(obj);
     }
 
 }
